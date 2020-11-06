7273c388688afaf9990c1e0e18b60f42abda8c7e
吉川 幸弘 <yukihiro.yoshikawa@consallink.co.jp>
2020-11-06 17:58:46 +0000

    エラー画面をカスタマイズ

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,42 +1,58 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
+    <v-container class="pt-0">
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="6" class="text-center">
+          <down-paimon></down-paimon>
+          <sorry-text></sorry-text>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="6" class="text-center">
+          <template v-if="$route.path === '/'">
+            <v-btn class="my-4" @click="reload">reload</v-btn>
+            <p v-if="!microphoneOK">
+              <v-icon left color="amber">mdi-lightbulb-on</v-icon>
+              <template v-if="microphoneAccess === ''"
+                >マイクが利用出来ないブラウザの可能性があります</template
+              >
+              <template v-else-if="microphoneAccess === 'denied'"
+                >マイクが許可されていないようです</template
+              >
+            </p>
+          </template>
+          <v-btn v-else nuxt to="/">To Home page</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
-<script>
-export default {
-  layout: 'empty',
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { microphoneOK, microphoneAccess } from '@/functions/useMicrophone'
+import DownPaimon from '@/components/DownPaimon.vue'
+import SorryText from '@/components/SorryText.vue'
+
+export default defineComponent({
+  components: {
+    DownPaimon,
+    SorryText,
+  },
   props: {
     error: {
       type: Object,
       default: null,
     },
   },
-  data() {
+  setup() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
+      microphoneOK,
+      microphoneAccess,
+      reload: () => {
+        if (process.browser) location.reload()
+      },
     }
   },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title,
-    }
-  },
-}
+})
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
